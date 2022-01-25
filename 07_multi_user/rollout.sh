a6bba3f14fc36b1fddbcfc48ccc722cccb0e74bb
Ed Espino <eespino@vmware.com>
2022-01-25 11:48:59 -0800

    Skip multi user processing if MULTI_USER_COUNT=0.

--- 07_multi_user/rollout.sh
+++ 07_multi_user/rollout.sh
@@ -15,11 +15,6 @@ if [[ "$GEN_DATA_SCALE" == "" || "$EXPLAIN_ANALYZE" == "" || "$RANDOM_DISTRIBUTI
         exit 1
 fi
 
-if [ "$MULTI_USER_COUNT" -eq "0" ]; then
-	echo "MULTI_USER_COUNT set at 0 so exiting..."
-	exit 0
-fi
-
 PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $PWD/../functions.sh
 source_bashrc
@@ -37,8 +32,11 @@ get_file_count()
 	file_count=$(ls $PWD/../log/end_testing* 2> /dev/null | wc -l)
 }
 
-get_file_count
-if [ "$file_count" -ne "$MULTI_USER_COUNT" ]; then
+if [ "$MULTI_USER_COUNT" -eq "0" ]; then
+    echo "MULTI_USER_COUNT set at 0 so exiting..."
+else
+    get_file_count
+    if [ "$file_count" -ne "$MULTI_USER_COUNT" ]; then
 	rm -f $PWD/../log/end_testing_*.log
 	rm -f $PWD/../log/testing*.log
 	rm -f $PWD/../log/rollout_testing_*.log
@@ -48,23 +46,23 @@ if [ "$file_count" -ne "$MULTI_USER_COUNT" ]; then
 	echo "cd $PWD/queries"
 	cd $PWD/queries
 	for i in $(seq 1 $MULTI_USER_COUNT); do
-		sql_dir="$PWD"/../tpch/"$i"
-		echo "checking for directory $sql_dir"
-		if [ ! -d "$sql_dir" ]; then
-			echo "mkdir -p $sql_dir"
-			mkdir -p $sql_dir
-		fi
-		echo "rm -f $sql_dir/*.sql"
-		rm -f $sql_dir/*.sql
-		echo "./qgen -p $i -c -v > $sql_dir/multi.sql"
-		./qgen -p $i -c -v > $sql_dir/multi.sql
+	    sql_dir="$PWD"/../tpch/"$i"
+	    echo "checking for directory $sql_dir"
+	    if [ ! -d "$sql_dir" ]; then
+		echo "mkdir -p $sql_dir"
+		mkdir -p $sql_dir
+	    fi
+	    echo "rm -f $sql_dir/*.sql"
+	    rm -f $sql_dir/*.sql
+	    echo "./qgen -p $i -c -v > $sql_dir/multi.sql"
+	    ./qgen -p $i -c -v > $sql_dir/multi.sql
 	done
 	cd ..
 
 	for x in $(seq 1 $MULTI_USER_COUNT); do
-		session_log=$PWD/../log/testing_session_$x.log
-		echo "$PWD/test.sh $x $EXPLAIN_ANALYZE"
-		$PWD/test.sh $x $EXPLAIN_ANALYZE > $session_log 2>&1 < $session_log &
+	    session_log=$PWD/../log/testing_session_$x.log
+	    echo "$PWD/test.sh $x $EXPLAIN_ANALYZE"
+	    $PWD/test.sh $x $EXPLAIN_ANALYZE > $session_log 2>&1 < $session_log &
 	done
 
 	sleep 60
@@ -73,9 +71,9 @@ if [ "$file_count" -ne "$MULTI_USER_COUNT" ]; then
 	echo "Now executing queries. This make take a while."
 	echo -ne "Executing queries."
 	while [ "$psql_count" -gt "0" ]; do
-		echo -ne "."
-		sleep 60
-		get_psql_count
+	    echo -ne "."
+	    sleep 60
+	    get_psql_count
 	done
 	echo "queries complete"
 	echo ""
@@ -83,10 +81,11 @@ if [ "$file_count" -ne "$MULTI_USER_COUNT" ]; then
 	get_file_count
 
 	if [ "$file_count" -ne "$MULTI_USER_COUNT" ]; then
-		echo "The number of successfully completed sessions is less than expected!"
-		echo "Please review the log files to determine which queries failed."
-		exit 1
+	    echo "The number of successfully completed sessions is less than expected!"
+	    echo "Please review the log files to determine which queries failed."
+	    exit 1
 	fi
+    fi
 fi
 
 end_step $step
--- 08_multi_user_reports/rollout.sh
+++ 08_multi_user_reports/rollout.sh
@@ -8,34 +8,37 @@ step="multi_user_reports"
 
 init_log $step
 
-get_version
-if [[ "$VERSION" == *"gpdb"* ]]; then
-	filter="gpdb"
-elif [ "$VERSION" == "postgresql" ]; then
-	filter="postgresql"
+if [ "$MULTI_USER_COUNT" -eq "0" ]; then
+    echo "MULTI_USER_COUNT set at 0 so exiting..."
 else
+    get_version
+    if [[ "$VERSION" == *"gpdb"* ]]; then
+	filter="gpdb"
+    elif [ "$VERSION" == "postgresql" ]; then
+	filter="postgresql"
+    else
 	echo "ERROR: Unsupported VERSION!"
 	exit 1
-fi
+    fi
 
-for i in $(ls $PWD/*.$filter.*.sql); do
+    for i in $(ls $PWD/*.$filter.*.sql); do
 	echo "psql -v ON_ERROR_STOP=1 -a -f $i"
 	psql -v ON_ERROR_STOP=1 -a -f $i
 	echo ""
-done
+    done
 
-filename=$(ls $PWD/*.copy.*.sql)
+    filename=$(ls $PWD/*.copy.*.sql)
 
-for i in $(ls $PWD/../log/rollout_testing_*); do
+    for i in $(ls $PWD/../log/rollout_testing_*); do
 	logfile="'""$i""'"
-	
         echo "psql -v ON_ERROR_STOP=1 -a -f $filename -v LOGFILE=\"$logfile\""
         psql -v ON_ERROR_STOP=1 -a -f $filename -v LOGFILE="$logfile"
-done
+    done
 
-psql -v ON_ERROR_STOP=1 -t -A -c "select 'analyze ' || n.nspname || '.' || c.relname || ';' from pg_class c join pg_namespace n on n.oid = c.relnamespace and n.nspname = 'tpch_testing'" | psql -v ON_ERROR_STOP=1 -t -A -e
+    psql -v ON_ERROR_STOP=1 -t -A -c "select 'analyze ' || n.nspname || '.' || c.relname || ';' from pg_class c join pg_namespace n on n.oid = c.relnamespace and n.nspname = 'tpch_testing'" | psql -v ON_ERROR_STOP=1 -t -A -e
 
-psql -v ON_ERROR_STOP=1 -F $'\t' -A -P pager=off -f $PWD/detailed_report.sql
-echo ""
+    psql -v ON_ERROR_STOP=1 -F $'\t' -A -P pager=off -f $PWD/detailed_report.sql
+    echo ""
+fi
 
 end_step $step
